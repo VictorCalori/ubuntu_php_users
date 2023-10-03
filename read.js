9723aa51f6c27b36d66748b3239e1f0a6eb94596
Calorithefacto <125594287+VictorCalori@users.noreply.github.com>
2023-10-03 15:20:30 -0300

    Add files via upload

---
+++ read.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+var con = mysql.createConnection({
+host: "localhost",
+user: "phpmyadmin",
+password: "aluno",
+database: "mydb"
+});
+con.connect(function(err) {
+if (err) throw err;
+con.query("SELECT * FROM customers LIMIT 10", function (err, result, fields) {
+if (err) throw err;
+console.log(result);
+});
+});
+
+
+
